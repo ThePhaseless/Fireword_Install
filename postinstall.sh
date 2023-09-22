241d911786ef3f8e5f850e864e8b951846f424e1
ThePhaseless <kukubaorch@gmail.com>
2023-09-22 22:43:49 +0200

    added samba, rclone and tailscale, udpated zsh

--- postinstall.sh
+++ postinstall.sh
@@ -8,46 +8,168 @@
 # Date:   September 18, 2023
 #
 
-# Update the package list and upgrade existing packages
-sudo apt update
-sudo apt upgrade -y
+# Make sure that environment variables are set
 
-# Show discs to the user and ask which discs should be formated with btrfs and prepared to be used with RAID0
+default=0
+if [ -z "$RAID0_DIR" ]; then
+  RAID0_DIR="/public/HDD"
+  default=True
+fi
+
+if [ -z "$CONFIG_DIR" ]; then
+  CONFIG_DIR="/public/SSD/Config"
+  default=True
+fi
+
+if [ -z "$MEDIA_DIR" ]; then
+  MEDIA_DIR="$RAID0_DIR/Media"
+  default=True
+fi
+
+# Ask the user if the default environment variables should be used
+if [ "$default" = True ]; then
+  echo "The default environment variables are:"
+  echo "RAID0_DIR=$RAID0_DIR"
+  echo "CONFIG_DIR=$CONFIG_DIR"
+  echo "MEDIA_DIR=$MEDIA_DIR"
+  read -p "Do you want to use the default environment variables? (Y/n) " answer
+  case $answer in
+  [Nn]*)
+    echo "Please set the environment variables and run the script again"
+    exit
+    ;;
+  *) ;;
+  esac
+fi
+
+# Make sure that the RAID0 directory exists
+if [ ! -d "$RAID0_DIR" ]; then
+  mkdir -p $RAID0_DIR
+fi
+
+# Make sure that the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root"
+  exit 1
+fi
+
+# Update the package list and upgrade existing packages
+apt update
+apt upgrade -y
+
+# Turn off screen after 5 mins of inactivity
+echo "Adding GRUB screen timeout..."
+sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=300/' /etc/default/grub
+update-grub
+echo "Done..."
+
+# Show disks to the user and ask which disks should be formated with btrfs and prepared to be used with RAID0 mounted to RAID0_DIR environment variable
+# Don't worry, it will be backed up daily to the cloud
 lsblk
-echo "Which discs should be formated and prepared to be used with RAID0? (e.g., sda sdb sdc)"
-read -p "Discs: " DISCS
-for DISC in $DISCS; do
-    sudo mkfs.btrfs -f /dev/$DISC
-    sudo mkdir -p /mnt/$DISC
-    sudo mount /dev/$DISC /mnt/$DISC
+echo "Which disks should be formated and prepared to be used with RAID0? (e.g., sda sdb sdc, none)"
+read -p "Disks: " DISKS
+for DISK in $DISKS; do
+  if [ "$DISK" = "none" ]; then
+    break
+  fi
+  mkfs.btrfs -f /dev/$DISK
+  mkdir -p $RAID0_DIR
+  mount /dev/$DISK $RAID0_DIR
 done
 
+# Install and config rclone
+echo "Installing rclone..."
+sudo -v
+curl https://rclone.org/install.sh | sudo bash
+
+# Configure rclone
+rclone config create gdrive drive
+
+# Add rclone to crontab daily
+echo "Adding rclone to crontab..."
+crontab -l | {
+  cat
+  echo "0 0 * * * rclone sync/public gdrive:Backup"
+} | crontab -
+
+# Preparing SAMBA
+echo "Preparing SAMBA..."
+apt install samba -y
+
+echo "Creating SAMBA shares..."
+printf "[HDD]\n
+    path = $RAID0_DIR\n
+    acl support = yes\n
+    read only = no\n
+    guest ok = yes\n
+    browsable = yes\n
+    writeable = yes\n
+    public = yes\n
+    " | tee -a /etc/samba/smb.conf
+mkdir -p $RAID0_DIR
+chown nobody:nogroup $RAID0_DIR
+chmod 777 $RAID0_DIR
+
+printf "[SSD]\n
+    path = /public/SSD\n
+    acl support = yes\n
+    read only = no\n
+    guest ok = yes\n
+    browsable = yes\n
+    writeable = yes\n
+    public = yes\n
+    " | tee -a /etc/samba/smb.conf
+mkdir -p /public/SSD
+chown nobody:nogroup /public/SSD
+chmod 777 /public/SSD
+
+echo "Restarting SAMBA..."
+systemctl restart smbd
+echo "Done..."
+
+# Install dependencies
+echo "Installing dependencies..."
+apt install curl git rsync gh -y
+echo "Done..."
+
+# Install Tailscale
+echo "Installing Tailscale..."
+curl -fsSL https://tailscale.com/install.sh | sh
+echo "Done..."
+
+tailscale up
+
 # Install Zsh and Oh-My-Zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Set Zsh as the default shell
 chsh -s $(which zsh)
 
-# Switch to the Zsh shell
-zsh
-
-# Install a nice Zsh theme (e.g., 'agnoster')
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+# Configure Zsh
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="refined"/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git docker docker-compose catimg colored-man-pages colorize command-not-found compleat cp extract gh zsh-interactive-cd vscode ubuntu timer themes last-working-dir thefuck systemadmin screen safe-paste python pip)/' ~/.zshrc
+echo "Done..."
 
 # Install Docker
+echo "Installing Docker and Docker Compose..."
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-# Install Docker Compose
-sudo apt install docker-compose -y
+sh get-docker.sh
+rm get-docker.sh
+echo "Done..."
 
 # Pull and run Portainer
+echo "Pulling and running Portainer..."
 docker volume create portainer_data
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-
-# Install Git
-sudo apt install git -y
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $CONFIG_DIR/Portainer:/data portainer/portainer-ce
+echo "Done..."
 
 # Clean up unnecessary packages
-sudo apt autoremove -y
+echo "Cleaning up unnecessary packages..."
+apt autoremove -y
+echo "Done..."
+
+echo "Post-Installation Script finished successfully!"
+echo "The system will reboot in 5 seconds..."
+sleep 5
+reboot
--- readme.md
+++ readme.md
@@ -1,12 +1,13 @@
 # MyPostInstall
 ## Description
-This is a script that I use to install my favorite programs on my NAS machines. It is a work in progress and will be updated as I find new programs that I like. I am currently using this on Ubuntu. Uses 
+This is a script that I use to install my favorite programs on my NAS machines. It is a work in progress and will be updated as I find new programs that I like. I am currently using this on Ubuntu. Uses
 
 ## Usage
 ### Environment Variables
 The following environment variables are used in this script:
-- `CONFIG_DIR` - The directory to install programs to. Defaults to `$HOME/dockerConfig`
-- `MEDIA_DIR` - The directory to store media files. Defaults to `$HOME/media`
+- `$CONFIG_DIR` - The directory to install programs to. Defaults to `/public/SSD/Config`
+- `$MEDIA_DIR` - The directory to store media files. Defaults to `$RAID0_DIR/Media`
+- `$RAID0_DIR` - The directory to store RAID0 files. Defaults to `/public/HDD`
 
 ### Running the Script
 To use this script, simply run the following command:
@@ -16,4 +17,3 @@ wget https://raw.githubusercontent.com/ThePhaseless/MyPostInstall/master/install
 
 ### General
 - [x] [Git](https://git-scm.com/)
-- [x] [Vim](https://www.vim.org/)
