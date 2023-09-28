6496a4a4d7b99ee86fe75e9e70fb7fddacebcc04
ThePhaseless <kukubaorch@gmail.com>
2023-09-29 00:35:56 +0200

    architecture check

--- install_vscode.sh
+++ install_vscode.sh
@@ -2,9 +2,25 @@
 echo "Downloading VSCode CLI..."
 sudo apt update
 sudo apt install git -y
-curl -o vscode_cli_alpine_x64_cli.tar.gz "$(curl -s -L -I -o /dev/null -w '%{url_effective}' "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64")"
-tar -xvzf vscode_cli_alpine_x64_cli.tar.gz
-rm vscode_cli_alpine_x64_cli.tar.gz
+
+# Check architecture if arm or x86
+echo "Checking architecture..."
+if [ "$(uname -m)" = "x86_64" ]; then
+    echo "Architecture is x86_64..."
+    architecture="x64"
+
+elif [ "$(uname -m)" = "aarch64" ]; then
+    echo "Architecture is armv7l..."
+    architecture="arm64"
+
+else
+    echo "Architecture is not supported..."
+    exit
+fi
+
+curl -o vscode_cli.tar.gz "$(curl -s -L -I -o /dev/null -w '%{url_effective}' "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-$architecture")"
+tar -xvzf vscode_cli.tar.gz
+rm vscode_cli.tar.gz
 
 # Move to bin
 echo "Moving VSCode CLI to /usr/local/bin..."
@@ -16,7 +32,7 @@ sudo chmod 777 /usr/local/bin/code
 
 # Install service
 echo "Installing VSCode service..."
-cp ./code.service /etc/systemd/system/code.service
+sudo cp ./code.service /etc/systemd/system/code.service
 
 # Reload services
 echo "Reloading services..."
