6949110d077bafa0d5f519bb827415c83f4141e5
ThePhaseless <kukubaorch@gmail.com>
2023-09-29 21:19:22 +0200

    docker non sudo

--- install.sh
+++ install.sh
@@ -275,8 +275,8 @@ echo "Done..."
 
 # Pull and run Portainer
 echo "Pulling and running Portainer..."
-sudo docker volume create portainer_data
-sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $CONFIG_PATH/Portainer:/data portainer/portainer-ce
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $CONFIG_PATH/Portainer:/data portainer/portainer-ce
 echo "Done..."
 
 # Allow for sudo without password
