fa3d96f0bad71ff38f4705c8ff94d660d55f87bb
ThePhaseless <kukubaorch@gmail.com>
2023-09-30 19:48:13 +0200

    samba better config

--- install.sh
+++ install.sh
@@ -185,37 +185,39 @@ sudo apt install samba wsdd -y
 
 echo "Creating SAMBA shares..."
 # Add HDD_SAMBA_SHARE to config file
-HDD_SAMBA_SHARE="[HDD]\n
-    path = $JBOD_PATH\n
-    acl support = yes\n
-    read only = no\n
-    guest ok = yes\n
-    browsable = yes\n
-    writeable = yes\n
-    public = yes\n
-    "
-# Check if the HDD_SAMBA_SHARE is already in the config file
-if grep -Fxq "$HDD_SAMBA_SHARE" /etc/samba/smb.conf; then
+HDD_SAMBA_SHARE="\n
+[HDD]
+    path = $JBOD_PATH
+    acl support = yes
+    read only = no
+    guest ok = yes
+    browsable = yes
+    writeable = yes
+    public = yes"
+# Check if the HDD_SAMBA_SHARE is already in the config file and add it if not
+if grep -Fxq "[HDD]" /etc/samba/smb.conf; then
   echo "HDD_SAMBA_SHARE is already in the config file"
 else
-  printf "$HDD_SAMBA_SHARE" | sudo tee -a /etc/samba/smb.conf
+  echo "Adding HDD_SAMBA_SHARE to the config file..."
+  echo -e $HDD_SAMBA_SHARE | sudo tee -a /etc/samba/smb.conf
 fi
 
 # Add SSD_SAMBA_SHARE to config file
-SSD_SAMBA_SHARE="[SSD]\n
-    path = \n
-    acl support = yes\n
-    read only = no\n
-    guest ok = yes\n
-    browsable = yes\n
-    writeable = yes\n
-    public = yes\n
-    "
-# Check if the SSD_SAMBA_SHARE is already in the config file
-if grep -Fxq "$SSD_SAMBA_SHARE" /etc/samba/smb.conf; then
+SSD_SAMBA_SHARE="\n
+[SSD]
+    path = $SSD_PATH
+    acl support = yes
+    read only = no
+    guest ok = yes
+    browsable = yes
+    writeable = yes
+    public = yes"
+# Check if the SSD_SAMBA_SHARE is already in the config file and add it if not
+if grep -Fxq "[SSD]" /etc/samba/smb.conf; then
   echo "SSD_SAMBA_SHARE is already in the config file"
 else
-  printf "$SSD_SAMBA_SHARE" | sudo tee -a /etc/samba/smb.conf
+  echo "Adding SSD_SAMBA_SHARE to the config file..."
+  echo -e $SSD_SAMBA_SHARE | sudo tee -a /etc/samba/smb.conf
 fi
 
 echo "Restarting SAMBA..."
