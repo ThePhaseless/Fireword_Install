722afe4f3a57212be0b4d7746c4129a6f9308e69
ThePhaseless <kukubaorch@gmail.com>
2023-10-16 17:02:08 +0200

    raid0 creator update

--- setup_raid0.sh
+++ setup_raid0.sh
@@ -29,42 +29,51 @@ else
     JBOD_disks=""
     for DISK in $DISKS; do
         # Check if disk exists
-        if [ ! -e "/dev/$DISK" ]; then
-            echo "Disk /dev/$DISK does not exist..."
+        if [ ! -e "/dev/${DISK}" ]; then
+            echo "Disk /dev/${DISK} does not exist..."
             exit 1
         fi
 
         # Check if disk is mounted
-        if grep -qs "/dev/$DISK" /proc/mounts; then
-            echo "Disk /dev/$DISK is mounted..."
+        if grep -qs "/dev/${DISK}" /proc/mounts; then
+            echo "Disk /dev/${DISK} is mounted..."
             exit 1
         fi
 
-        JBOD_disks="$JBOD_disks /dev/$DISK"
+        # Check if last character is a number (partition)
+        if [[ "${DISK:(-1)}" =~ ^[0-9]+$ ]]; then
+            echo "Disk /dev/${DISK} is a partition..."
+            echo "Adding to JBOD..."
+            JBOD_disks="$JBOD_disks /dev/${DISK}"
+        else
+            # Remove all partitions
+            echo "Removing existing partitions from /dev/${DISK}..."
+            wipefs -a /dev/${DISK}
+
+            # Create partition
+            echo "Creating partition..."
+            parted -s /dev/${DISK} mklabel gpt mkpart primary ext4 0% 100%
+            sleep 1
+
+            # Add partition to JBOD
+            echo "Adding to JBOD..."
+            JBOD_disks="$JBOD_disks /dev/${DISK}1"
+        fi
         JBOD_disks_num=$((JBOD_disks_num + 1))
     done
+
     # Create JBOD
+    echo "Creating RAID0..."
     mdadm --create --verbose /dev/md0 --level=0 --raid-devices=$JBOD_disks_num $JBOD_disks
+
     # Create filesystem
+    echo "Creating filesystem..."
     mkfs.ext4 -F /dev/md0
+
     # Mount JBOD
+    echo "Mounting JBOD..."
     mkdir -p $JBOD_PATH
     mount /dev/md0 $JBOD_PATH
-
-    # Check if JBOD config is already in mdadm.conf
-    if grep -q "# JBOD" /etc/mdadm/mdadm.conf; then
-        echo "JBOD already in mdadm.conf..."
-        echo "Removing old JBOD from mdadm.conf..."
-        # Remove old JBOD from mdadm.conf
-        sudo sed -i '/# JBOD/,/# JBOD END/d' /etc/mdadm/mdadm.conf
-    fi
-    echo "Adding JBOD to mdadm.conf..."
-    # Add JBOD to mdadm.conf
-    echo "# JBOD" | sudo tee -a /etc/mdadm/mdadm.conf
-    sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
-    echo "# JBOD END" | sudo tee -a /etc/mdadm/mdadm.conf
-
-    sudo update-initramfs -u
 
     # Check if JBOD config is already in fstab
     if grep -q "# JBOD" /etc/fstab; then
@@ -75,11 +84,18 @@ else
     fi
 
     echo "Adding JBOD to fstab..."
+    # Find UUID of JBOD
+    echo "Finding UUID of JBOD..."
+    JBOD_UUID=$(blkid -s UUID -o value /dev/md0)
+    echo "UUID of JBOD: $JBOD_UUID"
+
     # Add JBOD to fstab
+    echo "Adding JBOD to fstab..."
     echo "# JBOD" | tee -a /etc/fstab
     echo "# DO NOT EDIT THIS SECTION BY HAND" | tee -a /etc/fstab
-    echo "/dev/md0 $JBOD_PATH ext4 defaults,nofail,discard 0 0" | tee -a /etc/fstab
+    echo "UUID=$JBOD_UUID $JBOD_PATH ext4 defaults,nofail,discard 0 0" | tee -a /etc/fstab
     echo "# JBOD END" | tee -a /etc/fstab
+
     # Update initramfs
     echo "Updating initramfs..."
     sudo update-initramfs -u
