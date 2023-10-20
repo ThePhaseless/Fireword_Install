4a5d67e9e3ee0657655646294dfd2986e0a6e0fe
ThePhaseless <kukubaorch@gmail.com>
2023-10-21 00:18:47 +0200

    typo

--- Scripts/update_traefik_conf.sh
+++ Scripts/update_traefik_conf.sh
@@ -2,26 +2,26 @@
 # Pull and run Portainer
 
 # Import git links from setting.env
-source ./setting.conf
+source ./settings.conf
 
 # Import Category env for Traefik
 source ./Host/traefik.env
 
 # Check if running on proxy server or if --proxy flag is set
 if [ -z "$PROXY" || "$PROXY" == "false" ]; then
-    echo "PROXY is not set, using Host configuration"
 
-    echo "Pulling Traefik configs"
+    echo "PROXY is not set, using Host configuration"
+    echo "Pulling Traefik configs to $CONFIG_PATH/$CATEGORY/config"
     git clone $traefikRepo $CONFIG_PATH/$CATEGORY/config
-    git pull
+    git pull $CONFIG_PATH/$CATEGORY/config
 
     echo "Done"
 else
     echo "PROXY is set, using Proxy configuration"
 
-    echo "Pulling Traefik configs"
+    echo "Pulling Traefik configs from $traefikRepo to ./Proxy/Traefik/config"
     git clone $traefikRepo ./Proxy/Traefik/config
-    git pull
+    git pull ./Proxy/Traefik/config
 
     echo "Done"
 fi
